7ccc65c910d72cd38e7c4cc6dfecf36ddca19dd0
graceelliott8374-tech <graceelliott8374@gmail.com>
2026-07-29 14:08:15 -0400

    Initial commit

---
+++ src/Components/Header/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <>
+        <header>
+            <h1 className="glow">Ask The Magic Eight Ball!</h1>
+        </header>
+        </>
+    )
+}
